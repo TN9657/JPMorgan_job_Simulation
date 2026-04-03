437f0b3dc4fbfbbe797f3f73b32f936b552b3e7a
tanmay <tanmayai123@gmail.com>
2026-04-03 16:40:40 +0530

    commited

--- forage-midas/src/main/java/com/jpmc/midascore/MidasCoreApplication.java
+++ forage-midas/src/main/java/com/jpmc/midascore/MidasCoreApplication.java
@@ -1,5 +1,5 @@
 package com.jpmc.midascore;
-
+//Hellow my name is Tanmay Nirgude
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
